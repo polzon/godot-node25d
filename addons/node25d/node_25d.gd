a83e023246a626c15a325b8202a7da874b6432bb
Zack Polson <zack@polson.dev>
2026-04-06 15:35:23 -0600

    cleaned up node25d and added type safety
    Signed-off-by: Zack Polson <zack@polson.dev>

--- addons/node25d/node_25d.gd
+++ addons/node25d/node_25d.gd
@@ -1,25 +1,27 @@
-# This node converts a 3D position to 2D using a 2.5D transformation matrix.
-# The transformation of its 2D form is controlled by its 3D child.
 @tool
 @icon("res://addons/node25d/icons/node_25d.svg")
 class_name Node25D
 extends Node2D
+## This node converts a 3D position to 2D using a 2.5D transformation matrix.
+## The transformation of its 2D form is controlled by its 3D child.
 
-# SCALE is the number of 2D units in one 3D unit. Ideally, but not
+## SCALE is the number of 2D units in one 3D unit. Ideally, but not
 ## necessarily, an integer.
-const SCALE = 32
+const SCALE: int = 32
+## Equal axis for 45 degree angles, used in some of the view modes.
+const INV_SQRT_2: float = 0.70710678118
+const HALF_SQRT_3: float = 0.86602540378
 
 # Exported spatial position for editor usage.
 @export var spatial_position: Vector3:
-	get:
-		# TODO: Manually copy the code from this method.
-		return get_spatial_position()
-	set(value):
-		# TODO: Manually copy the code from this method.
-		set_spatial_position(value)
+	get = get_spatial_position,
+	set = set_spatial_position
 
 # GDScript throws errors when Basis25D is its own structure.
 # There is a broken implementation in a hidden folder.
+#
+# (Update: I just removed the broken implementation, not sure the point lol.)
+#
 # https://github.com/godotengine/godot/issues/21461
 # https://github.com/godotengine/godot-proposals/issues/279
 
@@ -33,25 +35,26 @@ var _spatial_node: Node3D
 
 
 # These are separated in case anyone wishes to easily extend Node25D.
-func _ready():
+func _ready() -> void:
 	node25d_ready()
 
 
-func _process(_delta):
+func _process(_delta: float) -> void:
 	node25d_process()
 
 
 # Call this method in _ready, or before node25d_process is first ran.
-func node25d_ready():
-	_spatial_node = get_child(0)
-	# Changing the values here will change the default for all Node25D instances.
+func node25d_ready() -> void:
+	_spatial_node = get_child(0) as Node3D
+	# Changing the values here will change the
+	# default for all Node25D instances.
 	_basis_x = SCALE * Vector2(1, 0)
-	_basis_y = SCALE * Vector2(0, -0.70710678118)
-	_basis_z = SCALE * Vector2(0, 0.70710678118)
+	_basis_y = SCALE * Vector2(0, -INV_SQRT_2)
+	_basis_z = SCALE * Vector2(0, INV_SQRT_2)
 
 
 # Call this method in _process, or whenever the position of this object changes.
-func node25d_process():
+func node25d_process() -> void:
 	if _spatial_node == null:
 		return
 	_spatial_position = _spatial_node.position
@@ -63,61 +66,61 @@ func node25d_process():
 	global_position = flat_pos
 
 
-func get_basis():
+func get_basis() -> Array[Vector2]:
 	return [_basis_x, _basis_y, _basis_z]
 
 
-func get_spatial_position():
+func get_spatial_position() -> Vector3:
 	if not _spatial_node:
-		_spatial_node = get_child(0)
-	return _spatial_node.position
+		_spatial_node = get_child(0) as Node3D
+	return _spatial_node.position if _spatial_node else Vector3.ZERO
 
 
-func set_spatial_position(value):
+func set_spatial_position(value: Vector3) -> void:
 	_spatial_position = value
 	if _spatial_node:
 		_spatial_node.position = value
 	elif get_child_count() > 0:
-		_spatial_node = get_child(0)
+		_spatial_node = get_child(0) as Node3D
 
 
 ## Change the basis based on the view_mode_index argument.
 ## This can be changed or removed in actual games where you only need
 ## one view mode.
-func set_view_mode(view_mode_index):
+func set_view_mode(view_mode_index) -> void:
 	match view_mode_index:
-		0:  # 45 Degrees
+		0: # 45 Degrees
 			_basis_x = SCALE * Vector2(1, 0)
-			_basis_y = SCALE * Vector2(0, -0.70710678118)
-			_basis_z = SCALE * Vector2(0, 0.70710678118)
-		1:  # Isometric
-			_basis_x = SCALE * Vector2(0.86602540378, 0.5)
+			_basis_y = SCALE * Vector2(0, -INV_SQRT_2)
+			_basis_z = SCALE * Vector2(0, INV_SQRT_2)
+		1: # Isometric
+			_basis_x = SCALE * Vector2(HALF_SQRT_3, 0.5)
 			_basis_y = SCALE * Vector2(0, -1)
-			_basis_z = SCALE * Vector2(-0.86602540378, 0.5)
-		2:  # Top Down
+			_basis_z = SCALE * Vector2(-HALF_SQRT_3, 0.5)
+		2: # Top Down
 			_basis_x = SCALE * Vector2(1, 0)
 			_basis_y = SCALE * Vector2(0, 0)
 			_basis_z = SCALE * Vector2(0, 1)
-		3:  # Front Side
+		3: # Front Side
 			_basis_x = SCALE * Vector2(1, 0)
 			_basis_y = SCALE * Vector2(0, -1)
 			_basis_z = SCALE * Vector2(0, 0)
-		4:  # Oblique Y
+		4: # Oblique Y
 			_basis_x = SCALE * Vector2(1, 0)
-			_basis_y = SCALE * Vector2(-0.70710678118, -0.70710678118)
+			_basis_y = SCALE * Vector2(-INV_SQRT_2, -INV_SQRT_2)
 			_basis_z = SCALE * Vector2(0, 1)
-		5:  # Oblique Z
+		5: # Oblique Z
 			_basis_x = SCALE * Vector2(1, 0)
 			_basis_y = SCALE * Vector2(0, -1)
-			_basis_z = SCALE * Vector2(-0.70710678118, 0.70710678118)
+			_basis_z = SCALE * Vector2(-INV_SQRT_2, INV_SQRT_2)
 
 
 ## Used by YSort25D
-static func y_sort(a: Node25D, b: Node25D):
+static func y_sort(a: Node25D, b: Node25D) -> bool:
 	return a._spatial_position.y < b._spatial_position.y
 
 
-static func y_sort_slight_xz(a: Node25D, b: Node25D):
+static func y_sort_slight_xz(a: Node25D, b: Node25D) -> bool:
 	var a_xz_spatial := a._spatial_position.x + a._spatial_position.z
 	var a_index = a._spatial_position.y + 0.001 * a_xz_spatial
 
